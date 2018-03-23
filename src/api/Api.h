7215e4951b5e7f8d079525ae1c486e32df9ef7a7
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-23 00:35:27 -0400

    Update Api.h

--- src/api/Api.h
+++ src/api/Api.h
@@ -33,6 +33,19 @@ class Hashrate;
 class NetworkState;
 
 
+class Api
+{
+public:
+    static bool start();
+    static void release();
 
+    static char *get(const char *url, int *status);
+    static void tick(const Hashrate *hashrate);
+    static void tick(const NetworkState &results);
+
+private:
+    static ApiState *m_state;
+    static uv_mutex_t m_mutex;
+};
 
 #endif /* __API_H__ */
